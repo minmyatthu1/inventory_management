a495442bbb66d99863d25dd1d16ac87c76179f70
Min Myat Thu <virusmmt@live.com>
2021-02-18 14:49:02 +0630

    local setting + setting for deploy

--- buy_sell/settings.py
+++ buy_sell/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'buy_sell.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'buy-sell',
-#         'USER': 'djangouser',
-#         'PASSWORD': 'abc123',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'buy-sell',
+        'USER': 'djangouser',
+        'PASSWORD': 'abc123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
